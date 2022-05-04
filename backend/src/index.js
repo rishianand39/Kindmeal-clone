c923e26c95fb563e99d6234d560c6802ce2d4fc0
Rishi Anand <rishi.rn818@gmail.com>
2022-05-04 18:33:04 +0530

    register login is implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,10 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
+const authController=require("./controllers/Auth.controller")
 
-
-
+// REGISTER LOGIN
+app.use("/auth",authController);
 
 
 
