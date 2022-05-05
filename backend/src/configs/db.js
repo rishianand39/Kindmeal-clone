32e494d51c98ebc80361dac249a6123ff55ddcba
Rishi Anand <rishi.rn818@gmail.com>
2022-05-05 15:38:07 +0530

    register&login api ready

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -3,7 +3,7 @@ const moongose = require("mongoose");
 
 const connect=() =>{
     return  moongose.connect(
-        'mongodb://rishianand:rishi123456@cluster0-shard-00-00.mlwwm.mongodb.net:27017,cluster0-shard-00-01.mlwwm.mongodb.net:27017,cluster0-shard-00-02.mlwwm.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-v4hpuo-shard-0&authSource=admin&retryWrites=true&w=majority'
+        'mongodb://rishianand:rishi123456@cluster0-shard-00-00.mlwwm.mongodb.net:27017,cluster0-shard-00-01.mlwwm.mongodb.net:27017,cluster0-shard-00-02.mlwwm.mongodb.net:27017/kindmeal?ssl=true&replicaSet=atlas-v4hpuo-shard-0&authSource=admin&retryWrites=true&w=majority'
     );
 }
 
