aa5b3e7938c60536ad7e9bd9187f4a3efcfc7b6e
Rishi Anand <rishi.rn818@gmail.com>
2022-05-05 20:02:41 +0530

    hotpick product api ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ const express=require("express")
 const app=express()
 app.use(express.json())
 const userController=require("./controllers/Auth.controller");
-
+const hotpickController=require("./controllers/Hotpick.controller")
 
 
 app.use("/auth",userController);
-
+app.use("/hotpick",hotpickController)
 
 
 module.exports=app;
---
+++ backend/src/models/Hotpick.model.js
@@ -0,0 +1,19 @@
+
+const mongoose=require("mongoose")
+
+const ProductSchema= new mongoose.Schema({
+    img:{type:String,required:true},
+    title:{type:String,required:true,unique:true},
+    desc:{type:String,required:true,},
+    categories:{type:Array,required:true},
+    discount:{type:Number,required:true},
+    price:{type:Number,required:true},
+    location:{type:String,required:true},
+    rating:{type:Number,},
+    details:{type:String,},    
+},{
+    timestamps:true,
+    versionKey:false
+})
+
+module.exports=mongoose.model("Product",ProductSchema)
