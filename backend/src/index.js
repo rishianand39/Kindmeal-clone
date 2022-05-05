008de5470e843bbecb05bf0fffe39f326d26a7d5
Rishi Anand <rishi.rn818@gmail.com>
2022-05-05 15:41:06 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,7 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
-const authController=require("./controllers/Auth.controller")
 
-// REGISTER LOGIN
-app.use("/auth",authController);
 
 
 
