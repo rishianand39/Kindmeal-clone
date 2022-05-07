a57709278c7fd5009025dbc43c8f86eda2689641
akashbind12 <97519781+akashbind12@users.noreply.github.com>
2022-05-08 02:19:36 +0530

    conflict removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home"
 import MealDeal from "./pages/MealDeal"
 import KindMoments from "./pages/KindMoments"
 import MealDealDetail from "./pages/MealDealDetail"
-import Signup from "./pages/Signup"
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="/mealdeal" element={<MealDeal />}/>
         <Route path="/kindmoments" element={<KindMoments />} />
         <Route path="/mealdeal/:id" element={<MealDealDetail />} />
-        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </div>
   );
